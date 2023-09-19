9daf238a84faa3e52b2ab8462d9f33a8bfd95fd0
Rogério Hopka <65628990+RogerioHopka@users.noreply.github.com>
2023-09-19 13:49:10 -0300

    Cread the Person Class and its methods and its Gets and Sets. Created the toString method.

--- app/src/main/java/br/com/lista_de_espera/mvc/model/Pessoa.java
+++ app/src/main/java/br/com/lista_de_espera/mvc/model/Pessoa.java
@@ -1,4 +1,61 @@
 package br.com.lista_de_espera.mvc.model;
 
 public class Pessoa {
+
+    //3° Os atributos são aqui
+
+    private String primeiroNome;
+    private String sobreNome;
+
+
+
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneDeContato() {
+        return telefoneDeContato;
+    }
+
+    public void setTelefoneDeContato(String telefoneDeContato) {
+        this.telefoneDeContato = telefoneDeContato;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "primeiroNome='" + primeiroNome + '\'' +
+                ", sobreNome='" + sobreNome + '\'' +
+                ", cursoDesejado='" + cursoDesejado + '\'' +
+                ", telefoneDeContato='" + telefoneDeContato + '\'' +
+                '}';
+    }
+
+    private String cursoDesejado;
+    private String telefoneDeContato;
+
+
+    public Pessoa() {}
+
 }
--- app/src/main/java/br/com/lista_de_espera/mvc/view/MainActivity.java
+++ app/src/main/java/br/com/lista_de_espera/mvc/view/MainActivity.java
@@ -5,12 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import br.com.lista_de_espera.R;
+import br.com.lista_de_espera.mvc.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 1° Cria a variável
+    Pessoa pessoa;
+
+    String dadosPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+    // 2° Cria o objeto
+    pessoa = new Pessoa();
+
+
+    pessoa.setPrimeiroNome("Rogério");
+    pessoa.setSobreNome("Hopka");
+    pessoa.setCursoDesejado("Java");
+    pessoa.setTelefoneDeContato("99991234");
+
+
+    dadosPessoa = " O seu primeiro nome é:  ";
+    dadosPessoa += pessoa.getPrimeiroNome();
+    dadosPessoa += " e o seu sobre nome é:  ";
+    dadosPessoa += pessoa.getSobreNome();
+
+    dadosPessoa += " e o seu curso desejado é:  ";
+    dadosPessoa += pessoa.getCursoDesejado();
+
+    dadosPessoa += " e o seu telefone é:  ";
+    dadosPessoa += pessoa.getTelefoneDeContato();
+
+     }
 }
